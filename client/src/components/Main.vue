19cf19fd65b32af09c06263b4ebd810eaaedf361
HeeSeok-Kwon <simondodream@gmail.com>
2022-01-31 23:43:16 +0900

    main page settings

---
+++ client/src/components/Main.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="doc_wrap">
+    <Header />
+    <div class="container_wrap">
+      <div class="contents_wrap">
+        <div class="box_group_01">
+          <div class="box01">
+            <div class="visual">
+              <img alt="GDSC Symbol" src="../assets/gdscSymbol.png">
+            </div> 
+          </div> 
+          <div class="box03">
+            
+          </div> 
+        </div>  
+        <div class="box_group_02">
+          <div class="box02">
+            <div class="main_text">
+              Google Developer Students Club<br />
+              Pusan National University Chapter
+            </div>
+            <div class="sub_text">
+              <a href="#">
+                <img alt="GDSC Symbol" src="../assets/arrow.png">
+                <p>Introduce</p>
+              </a>
+            </div>
+          </div> 
+          <div class="box04">
+            
+          </div> 
+        </div>
+      </div>
+    </div>
+  </div>
+  
+    
+</template>
+
+<script>
+import Header from "./header";
+
+export default {
+  name: 'Main',
+  components: {
+    Header
+  },
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .doc_wrap {position:relative;overflow:hidden;min-width:1400px;}
+
+  .container_wrap {position: relative;display: block; margin-top: 93px;}
+  .contents_wrap {position: relative;display: block;background: #f9f9f9;width: 1400px;margin:0 auto;}
+
+  .box_group_01 {float:left;}
+  .box01 {width:400px;height:400px;}
+  .box01 div.visual img{margin:130px 0 0 50px; width:350px; height:200px;}
+
+  .box03 {height:300px;}
+
+  .box_group_02 {float:left;}
+  .box02 {width:1000px;height:400px; box-shadow: 0 10px 10px -10px rgba(11, 11, 11, 0.16);border-bottom: 3px solid #707070;}
+  .box02 div.main_text{text-align: left; font-size:40px; padding:240px 0 0 60px;}
+  .box02 div.sub_text a{text-decoration: none;}
+  .box02 div.sub_text p{font-size:35px;float:right;margin:10px 10px 0 0;}
+  .box02 div.sub_text img{float:right; width:40px; height:40px; margin:16px 0 0 0;}
+  
+  .box04 {height:300px;}
+</style>
---
+++ client/src/components/header.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="header_wrap">
+        <div class="header">
+            <ul class="logo">
+                <li class="logo_img">
+                    <img alt="Vue logo" src="../assets/google.png">
+                </li>
+                <li class="logo_txt">
+                    <div>Google Developer Students Club Pusan National Univ</div>
+                </li>
+            </ul>
+            <div class="navi-wrap">
+                <ul class="navi">
+                    <li class="nm-mn1">
+                        <a href="#">
+                            GDSC PNU 소개
+                        </a>
+                    </li>
+                    <li class="nm-mn1">
+                        <a href="#">
+                            구성원 소개
+                        </a>
+                    </li>
+                    <li class="nm-mn1">
+                        <a href="#">
+                            활동 내용
+                        </a>
+                    </li>
+                    <li class="nm-mn1">
+                        <a href="#">
+                            참여 신청
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>    
+</template>
+
+<script>
+    export default {
+        name: 'Header',
+    }
+</script>
+
+<style scoped>
+    .header_wrap {position: fixed;top: 0;left: 0;width: 100%;height: 93px;display: block;background: #fff; box-shadow: 0 5px 6px 0 rgba(0, 0, 0, 0.16); border-bottom: 1px solid #011557; z-index:500}
+    .header {position:relative;display:block;width: 1400px;height: 80px;margin: 0 auto;}
+    .header .logo {display: block;text-align: center; float: left;}
+    .header .logo li {position: relative;display: inline-block;vertical-align:middle;padding: 0 8px;}
+    .header .logo li.logo_img img {display: block;height:40px;line-height:80px;}
+    .header .logo li.logo_txt div {display: block;color:#808080;font-size:20px;line-height:80px;}
+
+    .header .navi-wrap {position:absolute;top:0;left:0;display:block;width: 100%;}
+
+    .header .navi {display: block;text-align: center; float: right;}
+    .header .navi li.nm-mn1 {position: relative;display: inline-block;vertical-align:top;padding: 0 20px;}
+    .header .navi li.nm-mn1 a{position: relative;display:block;color:#808080;font-size:20px;font-weight: 600;line-height:80px;height: 80px;text-align: center; text-decoration: none;}
+
+</style>
