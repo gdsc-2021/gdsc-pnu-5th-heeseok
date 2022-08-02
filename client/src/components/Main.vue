f0587272beb7eaa596a295fd4fdcdae048e6a981
HeeSeok-Kwon <simondodream@gmail.com>
2022-08-02 10:19:18 +0900

    docs: Header, Main page mod

--- client/src/components/Main.vue
+++ client/src/components/Main.vue
@@ -84,7 +84,7 @@ export default {
     .contents_wrap .box_wrap .box_text_wrap .box-router {height:calc(100% - 120px - 31.3px);}
     .contents_wrap .box_wrap .box_text_wrap .box-router a{color:#2e2d2d; text-decoration: none;}
     .contents_wrap .box_wrap .box_text_wrap .box-router p{font-size:27px;float:right;margin:7.5px 10px 0 0; text-shadow: 5px 5px 6px rgba(0, 0, 0, 0.16); font-family: AppleSDGothicNeo; font-weight: 300; font-stretch: normal; font-style: normal; line-height: normal; letter-spacing: normal; text-align: left;}
-    .contents_wrap .box_wrap .box_text_wrap .box-router img{float:right; width:45px; height:29px; margin: 15.5px 15px 0 0;}
+    .contents_wrap .box_wrap .box_text_wrap .box-router img{float:right; width:45px; height:29px; margin: 16.5px 15px 0 0;}
 
     .contents_wrap .box_wrap .box_text_wrap .box-line {width:100%; height: 2px; box-shadow: 20px 20px 6px 0 rgba(11, 11, 11, 0.16);background-color: #707070; margin-top: 65px;}
   }
@@ -101,7 +101,7 @@ export default {
     .contents_wrap .box_wrap .box_text_wrap .box-router {height:calc(100% - 120px - 31.3px);}
     .contents_wrap .box_wrap .box_text_wrap .box-router a{color:#2e2d2d; text-decoration: none;}
     .contents_wrap .box_wrap .box_text_wrap .box-router p{font-size:25px;float:right;margin:0px 10px 0 0; text-shadow: 5px 5px 6px rgba(0, 0, 0, 0.16); font-family: AppleSDGothicNeo; font-weight: 300; font-stretch: normal; font-style: normal; line-height: normal; letter-spacing: normal; text-align: left;}
-    .contents_wrap .box_wrap .box_text_wrap .box-router img{float:right; width:45px; height:29px; margin: 7.5px 15px 0 0;}
+    .contents_wrap .box_wrap .box_text_wrap .box-router img{float:right; width:45px; height:29px; margin: 8px 15px 0 0;}
 
     .contents_wrap .box_wrap .box_text_wrap .box-line {width:100%; height: 2px; box-shadow: 20px 20px 6px 0 rgba(11, 11, 11, 0.16);background-color: #707070; margin-top: 55px;}
   }
@@ -118,7 +118,7 @@ export default {
     .contents_wrap .box_wrap .box_text_wrap .box-router {height:calc(100% - 120px - 31.3px);}
     .contents_wrap .box_wrap .box_text_wrap .box-router a{color:#2e2d2d; text-decoration: none;}
     .contents_wrap .box_wrap .box_text_wrap .box-router p{font-size:22px;float:right;margin:10px 10px 0 0; text-shadow: 5px 5px 6px rgba(0, 0, 0, 0.16); font-family: AppleSDGothicNeo; font-weight: 300; font-stretch: normal; font-style: normal; line-height: normal; letter-spacing: normal; text-align: left;}
-    .contents_wrap .box_wrap .box_text_wrap .box-router img{float:right; width:45px; height:29px; margin: 11px 0px 0 0;}
+    .contents_wrap .box_wrap .box_text_wrap .box-router img{float:right; width:45px; height:29px; margin: 12px 0px 0 0;}
 
     .contents_wrap .box_wrap .box_text_wrap .box-line {width:100%; height: 2px; box-shadow: 20px 20px 6px 0 rgba(11, 11, 11, 0.16);background-color: #707070; margin-top: 55px;}
   }
